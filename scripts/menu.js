9ed3fc9abc482eb9c4f05beada07c2180072f7c5
Sifan <36680379+amandas08@users.noreply.github.com>
2018-11-26 00:33:26 -0800

    header/slideshow
    1. add hamburger to navigation bar
    2. add slideshow for index page
    3.finished course outline page

--- scripts/menu.js
+++ scripts/menu.js
@@ -6,7 +6,7 @@
 	// d = document
 	
 	const $body = d.querySelector('body');
-	const $btn = d.querySelector('.btn-menu');
+	const $btn = d.querySelector('.hamburger');
 	
 	$btn.addEventListener('click', function(){
 		
@@ -28,4 +28,19 @@ function popFunction() {
     var menu = document.getElementById("hamburger");
     nav.classList.toggle("show");
     
+}
+// automatic slides 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
 }
