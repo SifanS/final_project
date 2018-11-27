4b7620b9ebf75745003b48d024ab00270d2dee84
Sifan <36680379+amandas08@users.noreply.github.com>
2018-11-27 14:21:29 -0800

    first 3 pages finish 1

--- scripts/menu.js
+++ scripts/menu.js
@@ -17,18 +17,8 @@
 	
 })(document);
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-function popFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-    var menu = document.getElementById("hamburger");
-    nav.classList.toggle("show");
-    
-}
+
+
 // automatic slides 
 var myIndex = 0;
 carousel();
